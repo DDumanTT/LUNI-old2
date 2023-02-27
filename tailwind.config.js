f29242997663e7300517dc5bd395a2d5270d23b0
DDumanTT <domantas.stakionis@ktu.edu>
2023-02-28 01:44:10 +0200

    Add radix ui colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { toRadixVars } = require("windy-radix-palette/vars");
+
 module.exports = {
   content: ["./src/ui/**/*.{js,ts,jsx,tsx}", "./src/index.html"],
   darkMode: 'class',
@@ -7,8 +9,20 @@ module.exports = {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
         pressstart2p: ['PressStart2P', 'sans-serif']
+      },
+      colors: {
+        primary: toRadixVars('indigo'),
+        secondary: toRadixVars('red'),
+        neutral: toRadixVars('slate'),
+        success: toRadixVars('grass'),
+        warning: toRadixVars('yellow'),
+        error: toRadixVars('tomato'),
+        info: toRadixVars('cyan'),
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require("windy-radix-palette"),
+  ],
 }
